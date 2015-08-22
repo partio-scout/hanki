a3ecc9940b7faf49e578805a864c33d4960c3022
ritvje <jenna.ritvanen@gmail.com>
2015-08-22 13:02:22 +0300

    updated title.js & installed bluebird & added data to unknown titlegroup,account,supplier

--- common/models/title.js
+++ common/models/title.js
@@ -1,141 +1,123 @@
 module.exports = function(Title) {
     Title.DataImport = function(csv,cb){
-        // Title.destroyAll();
-        // cb(null, null);
-        var Q = require('q');
-        var loopback = require('loopback');
+        var Promise = require('bluebird');
+        // var loopback = require('loopback');
         var app = require('../../server/server');
         var Titlegroup = app.models.Titlegroup;
         var Account = app.models.Account;
         var Supplier = app.models.Supplier;
 
         function csvJSON(csv){
-        //  console.log(csv);
-          var lines = csv.split("\\n");
-        //  console.log(lines[0]);
-        //  console.log(lines);
+          // console.log(csv);
+          var lines = csv.split('\\n');
+          // console.log(lines[0]);
+          // console.log(lines);
           var result = [];
-          var headers = lines[0].split(",");
-        //    console.log(headers);
-          for(var i = 1; i < lines.length; i++){
-        	  var obj = {};
-        	  var currentline = lines[i].split(",");
-        	  for(var j = 0; j < headers.length; j++){
-        		  obj[headers[j]] = currentline[j];
-        	  }
-        	  result.push(obj);
+          var headers = lines[0].split(',');
+          // console.log(headers);
+          for (var i = 1; i < lines.length; i++){
+            	  var obj = {};
+            	  var currentline = lines[i].split(',');
+            	  for (var j = 0; j < headers.length; j++){
+              obj[headers[j]] = currentline[j];
+            	  }
+            	  result.push(obj);
           }
           return result; //JavaScript object
-        //   return JSON.stringify(result); //JSON
+          // return JSON.stringify(result); //JSON
         }
 
         function titlegroup_exists(obj){
-            return Q.Promise(function(resolve,reject){
-                Titlegroup.exists(obj.titlegroupId, function(err, bool){
-                    if (err) {
-                        reject(new Error("titlegroup_exists" + new Error (err)));
+          return new Promise(function(resolve,reject){
+              Titlegroup.exists(obj.titlegroupId, function(err, bool){
+                  if (err) {
+                    reject(new Error('titlegroup_exists ' + new Error (err)));
+                  } else {
+                    if (!bool) {
+                      obj.titlegroupId = 0;
+                      resolve(true);
                     }
-                    else {
-                        if (!bool) {
-                            obj.titlegroupId = 0;
-                            resolve(true);
-                        }
-                        else resolve(false);
-                    }
+                      else resolve(false);
+                  }
                 });
             });
         }
         function account_exists(obj){
-            return Q.Promise(function(resolve,reject){
+          return new Promise(function(resolve,reject){
                 Account.exists(obj.accountId, function(err, bool){
                     if (err) {
-                        reject(new Error("account_exists " + new Error (err)));
+                      reject(new Error('account_exists ' + new Error (err)));
+                    } else {
+                      if (!bool) {
+                        obj.accountId = 0;
+                        resolve(true);
+                      } else resolve(false);
                     }
-                    else {
-                        if (!bool) {
-                            obj.accountId = 0;
-                            resolve(true);
-                        }
-                        else resolve(false);
-                    }
-                });
-            });
+                  });
+              });
         }
         function supplier_exists(obj){
-            return Q.Promise(function(resolve,reject){
+          return new Promise(function(resolve,reject){
                 Supplier.exists(obj.supplierId, function(err, bool){
                     if (err) {
-                        reject(new Error("supplier_exists " + new Error (err)));
+                      reject(new Error('supplier_exists ' + new Error (err)));
+                    } else {
+                      if (!bool) {
+                        obj.supplierId = 0;
+                        resolve(true);
+                      } else resolve(false);
                     }
-                    else {
-                        if (!bool) {
-                            obj.supplierId = 0;
-                            resolve(true);
-                        }
-                        else resolve(false);
-                    }
-                });
-            });
+                  });
+              });
         }
         function title_create(obj){
-            return Q.Promise(function(resolve,reject){
+          return new Promise(function(resolve,reject){
                 Title.create(obj, function(err, obj){
                     if (err) {
-                        console.log(obj);
-                        reject(new Error("title_create " + new Error (err)));
+                      console.log(obj);
+                      reject(new Error('title_create ' + new Error (err)));
+                    } else {
+                      // console.log(obj);
+                      resolve(true);
                     }
-                    else {
-                        // console.log(obj);
-                        resolve(true);
-                    }
-                });
-            });
-        }
-        function checkTitle(obj){
-                return Q.all([
-                    titlegroup_exists(obj),
-                    account_exists(obj),
-                    supplier_exists(obj),
-                    obj])
-                    .then(function(array){
-                        title_create(obj)
-                            .then(function(obj){
-                                return;
-                            }, function(err){
-                                console.log('1',err);
-                                cb(err,null);
-                            });
-                        // console.log(array);
-                        // return true;
-                    }, function(err){
-                            console.log('2',err);
-                            cb(err,null);
-                    });
-        }
-        function isReady(){
-            index++;
-            console.log(index);
-            if (index == len) cb(null,'Tuotteesi on lisätty tietokantaan.');
+                  });
+              });
         }
 
         var result = csvJSON(csv);
         console.log(result);
-        var len = result.length;
-        var index = 0;
 
-        for (var i = 0; i < len; i++){
-            checkTitle(result[i])
-            .then(isReady());
+        Promise.each(result, function(obj){
+                return Promise.all([
+                  titlegroup_exists(obj),
+                  account_exists(obj),
+                  supplier_exists(obj),
+                  obj])
+                    .then(function(array){
+                        title_create(obj)
+                            .then(function(obj){
+                              return;
+                            }, function(err){
+                                console.log('1',err);
+                                cb(err,null);
+                              });
+                      }, function(err){
+                            console.log('2',err);
+                            cb(err,null);
+                          });
+              })
+                .then(function(result) {
+                    cb(null, result);
+                  });
 
-        }
-    };
+      };
 
     Title.remoteMethod(
         'DataImport',
-        {
-            http: {path: '/DataImport', verb: 'post'},
-            accepts: {arg: 'csv', type: 'string'},
-            returns: {arg: 'result', type: 'string'}
-        }
+      {
+        http: { path: '/DataImport', verb: 'post' },
+        accepts: { arg: 'csv', type: 'string' },
+        returns: { arg: 'result', type: 'string' }
+      }
     );
-};
+  };
--- data.sql
+++ data.sql
@@ -1,7 +1,10 @@
+INSERT INTO account (account_id,name,description) VALUES (0,'Tuntematon tili','Tuntematon tili: Tilinumero FIxxxyyyzzz');
 INSERT INTO account (name,description) VALUES ('Testitili 1','Testitili 1: Tilinumero FIxxxyyyzzz');
 INSERT INTO account (name,description) VALUES ('Testitili 2','Testitili 2: Tilinumero FIxxxyyyzzz');
+INSERT INTO titlegroup (titlegroup_id,name,description) VALUES (0,'Tuntematon','Ei tunneta ryhmää');
 INSERT INTO titlegroup (name,description) VALUES ('Rautatavara','Pultit ja mutterit');
 INSERT INTO titlegroup (name,description) VALUES ('Puutavara','Laudat ja lankut');
+INSERT INTO supplier (supplier_id,name,address,contact_person,phone,email,customer_number,business_id,terms_of_payment,terms_of_delivery,method_of_delivery) VALUES (0,'Tuntematon','Osoite tuntematon','Nimi tuntematon','000','tero@toimitta.ja','222','Y-222-222','Käteinen','Maksu ensin','Paikan päälle');
 INSERT INTO supplier (name,address,contact_person,phone,email,customer_number,business_id,terms_of_payment,terms_of_delivery,method_of_delivery) VALUES ('Tmi Toimittaja','Toimituskatu 1','Tero Toimittaja','123','tero@toimitta.ja','222','Y-222-222','Käteinen','Maksu ensin','Paikan päälle');
 INSERT INTO delivery (description,earliest,latest,address) VALUES ('Tavaroitten toimitus','2015-08-25','2015-08-27','Evo');
 INSERT INTO costcenter (name) VALUES ('Roihun rahat');
