40aa4597869ff786ef42128cc6c29d0c2312a6e3
Emil Virkki <emil.virkki@gmail.com>
2015-08-23 20:44:58 +0300

    Creat script that creates schema + initial roles from model descriptions

---
+++ server/migrate/create-schema.js
@@ -0,0 +1,31 @@
+var app = require('../server.js');
+var db = app.datasources.db;
+
+// The order of these models is important for database row creation!
+var modelsToAutoMigrate = [
+  'ACL',
+  'AccessToken',
+  'Role',
+  'RoleMapping',
+  'User',
+  'Title',
+  'Titlegroup',
+  'Account',
+  'Supplier',
+  'Delivery',
+  'Costcenter',
+  'Purchaseuser',
+  'Usageobject',
+  'Purchaseorder',
+  'Purchaseorderrow'
+];
+
+db.automigrate(modelsToAutoMigrate, function() {
+  var roleFixtures = require('../../common/fixtures/all/Role.json');
+  app.models.Role.create(roleFixtures, function(err, res) {
+  	console.log('Created default roles: ', err, res);
+  	db.disconnect();
+  });
+});
+
+console.log('Created following tables: ' + modelsToAutoMigrate + '. If they existed earlier, they were dropped and recreated.');
