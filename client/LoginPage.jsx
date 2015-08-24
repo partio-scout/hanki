dcd6798c0125610fcf96928b621cd013d5576fa1
Emil Virkki <emil.virkki@gmail.com>
2015-08-25 00:12:20 +0300

    Add placeholder code in frontend to display logged in user's email address

--- client/LoginPage.jsx
+++ client/LoginPage.jsx
@@ -5,22 +5,41 @@ var Row = ReactBootstrap.Row;
 var Col = ReactBootstrap.Col;
 var ButtonToolbar = ReactBootstrap.ButtonToolbar;
 var Button = ReactBootstrap.Button;
+var Alert = ReactBootstrap.Alert;
+
+// Yes, this is ugly - will fix later
+var Cookie = require('js-cookie');
+var accessToken = Cookie.getJSON('accessToken');
+var email = Cookie.getJSON('email');
 
 var LoginPage = React.createClass({
   render: function () {
+    var homeView;
+    if (accessToken) {
+      homeView = (
+        <Alert bsStyle="success">
+          Tervetuloa, { email }
+        </Alert>
+      );
+    } else {
+      homeView = (
+        <ButtonToolbar>
+          <Button
+            href="/saml/login"
+            bsStyle='primary'
+            bsSize='large'>
+            Kirjaudu sisään Partio ID:llä
+          </Button>
+        </ButtonToolbar>
+      );
+    }
+
     return (
       <Row>
         <Col>
           <h3>HANKI</h3>
           <p>Lorem ipsum dolor sit amet</p>
-          <ButtonToolbar>
-            <Button
-              href="/samllogin"
-              bsStyle='primary'
-              bsSize='large'>
-              Kirjaudu sisään Partio ID:llä
-            </Button>
-          </ButtonToolbar>
+          { homeView }
         </Col>
       </Row>
     );
