c30c573bda1e0fecb26cdc9f44da78a302843691
Emil Virkki <emil.virkki@gmail.com>
2015-08-30 15:29:45 +0300

    Fix linter errors

--- scripts/create-user.js
+++ scripts/create-user.js
@@ -1,11 +1,11 @@
 var path = require('path');
 var serverPath = path.resolve(__dirname, '../server');
-var app = require(serverPath + '/server.js')
+var app = require(serverPath + '/server.js');
 var crypto = require('crypto');
 
 var email = process.argv[2]; // 0=node, 1=create-user.js
 
-if(!email) {
+if (!email) {
   console.log('Please provide the user\'s email: node create-user.js <email>');
   process.exit(1);
 }
@@ -13,7 +13,7 @@ if(!email) {
 var password = crypto.randomBytes(24).toString('hex');
 
 app.models.User.create({ email: email, password: password }, function(err, user) {
-  if(err) {
+  if (err) {
     console.error('Can\'t create user:\n', err);
     process.exit(1);
   } else {
