4e3e42b0bfbe51d4a0da00414ef2233d9b8e3f2d
ritvje <jenna.ritvanen@gmail.com>
2015-09-04 14:01:29 +0300

    Change functions to check if objects properties exist

--- common/models/title.js
+++ common/models/title.js
@@ -1,45 +1,44 @@
 module.exports = function(Title) {
     Title.DataImport = function(csv,cb){
-        var Parse = require('csv-parse');
         var Promise = require('bluebird');
         var app = require('../../server/server');
+        var Parse = Promise.promisify(require('csv-parse'));
         var Titlegroup = app.models.Titlegroup;
         var Account = app.models.Account;
         var Supplier = app.models.Supplier;
-
+        var titlegroupId_exists = Promise.promisify(Titlegroup.exists, Titlegroup);
+        var accountId_exists = Promise.promisify(Account.exists, Account);
+        var supplierId_exists = Promise.promisify(Supplier.exists, Supplier);
+        var title_create = Promise.promisify(Title.create, Title);
+        // Check if objects property already exists in db; if not, change to 0
+        // obj: a Title-object, func: function to check if property exists, property: property to check & change to 0 if it doesn't exist
+        function checkIfPropertyExists(obj, func, property) {
+          // Check if passed function actually a function
+          if (func && (typeof func == 'function')){
+            return func(obj[property])
+            .then(function(exists){
+              if (!exists) {
+                // Property doesn't already exist, so change to 0
+                obj[property] = 0;
+              }
+            }, function(err) {
+              cb(err,null);
+            });
+          } else {
+            // passed function not a function
+            cb('faulty function', null);
+          }
+        };
         // options for Parse function:
         var options = { columns: true, skip_empty_lines: true, auto_parse: true, delimiter: ',', rowDelimiter: '\\n' };
-        function parseInput(csv, options) {
-          return new Promise(function(resolve, reject) {
-            Parse(csv, options, function(err, output) {
-              if (err) reject(new Error(err));
-              else {
-                resolve(output);
-              }
-            });
-          });
-        }
-        function title_create(obj){
-          return new Promise(function(resolve,reject){
-                Title.create(obj, function(err, obj){
-                    if (err) {
-                      console.log(obj.titleId);
-                      console.log(err);
-                      reject(new Error(err));
-                    } else {
-                      resolve(true);
-                    }
-                  });
-              });
-        }
         if (!csv) cb(null, '');
         else {
-          parseInput(csv, options)
+          Parse(csv, options)
             .each(function(obj){
                   return Promise.all([
-                    Titlegroup._exists(obj),
-                    Account._exists(obj),
-                    Supplier._exists(obj),
+                    checkIfPropertyExists(obj, titlegroupId_exists, 'titlegroupId'),
+                    checkIfPropertyExists(obj, accountId_exists, 'accountId'),
+                    checkIfPropertyExists(obj, supplierId_exists, 'supplierId'),
                     obj])
                       .then(function(array){
                           title_create(obj)
--- common/models/titlegroup.js
+++ common/models/titlegroup.js
@@ -1,18 +1,2 @@
 module.exports = function(Titlegroup){
-  // Check if input objects titlegroupId exists; if not, change it to zero
-  Titlegroup._exists = function(obj){
-    var Promise = require('bluebird');
-    return new Promise(function(resolve,reject){
-        Titlegroup.exists(obj.titlegroupId, function(err, bool){
-            if (err) {
-              reject(new Error(err));
-            } else {
-              if (!bool) {
-                obj.titlegroupId = 0;
-                resolve(true);
-              } else resolve(false);
-            }
-          });
-      });
-  };
 };
