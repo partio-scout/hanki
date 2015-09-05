f4708fc31d8cd42d957c69fd28c247e7cdeb6774
ritvje <jenna.ritvanen@gmail.com>
2015-09-05 22:35:35 +0300

    Use transaction to update db & change csv headers to not detected from cs

--- common/models/title.js
+++ common/models/title.js
@@ -10,53 +10,83 @@ module.exports = function(Title) {
         var accountId_exists = Promise.promisify(Account.exists, Account);
         var supplierId_exists = Promise.promisify(Supplier.exists, Supplier);
         var title_create = Promise.promisify(Title.create, Title);
+        var Title_beginTransaction = Promise.promisify(Title.beginTransaction, Title);
+        var AnyErrorsHappened = false;
         // Check if objects property already exists in db; if not, change to 0
         // obj: a Title-object, func: function to check if property exists, property: property to check & change to 0 if it doesn't exist
         function checkIfPropertyExists(obj, func, property) {
-          // Check if passed function actually a function
-          if (func && (typeof func == 'function')){
+          if (func && (typeof func == 'function')){ // Check if passed function actually a function
             return func(obj[property])
             .then(function(exists){
-              if (!exists) {
-                // Property doesn't already exist, so change to 0
+              if (!exists) { // Property doesn't already exist, so change to 0
                 obj[property] = 0;
               }
             }, function(err) {
               cb(err,null);
             });
-          } else {
-            // passed function not a function
-            cb('faulty function', null);
+          } else { // passed function not a function
+            cb('flawed function', null);
           }
         };
         // options for Parse function:
-        var options = { columns: true, skip_empty_lines: true, auto_parse: true, delimiter: ',', rowDelimiter: '\\n' };
-        if (!csv) cb(null, '');
-        else {
-          Parse(csv, options)
-            .each(function(obj){
-                  return Promise.all([
-                    checkIfPropertyExists(obj, titlegroupId_exists, 'titlegroupId'),
-                    checkIfPropertyExists(obj, accountId_exists, 'accountId'),
-                    checkIfPropertyExists(obj, supplierId_exists, 'supplierId'),
-                    obj])
-                      .then(function(array){
-                          title_create(obj)
-                              .then(function(obj){
-                                return;
-                              }, function(err){
-                                  cb(err,null);
-                                });
-                        }, function(err){
-                              cb(err,null);
-                            });
-                })
-                  .then(function(result) {
-                      cb(null, result);
+        var options = { columns: [ 'name', 'titlegroupId', 'unit', 'priceWithoutTax', 'vatPercent', 'priceWithTax', 'accountId', 'supplierId', 'supplierTitlecode', 'toResold', 'toRent', 'toBought', 'toSignedFor', 'memo', 'selectable' ], skip_empty_lines: true, auto_parse: true, delimiter: ',', rowDelimiter: '\\n' };
+        if (!csv || (csv === '')) { // input csv is empty; return empty string
+          cb(null, '');
+        } else {
+          var beginTx = Title_beginTransaction({ isolationLevel: Title.Transaction.READ_COMMITTED });
+          beginTx.then(function(tx) {
+            Parse(csv, options)
+              .each(function(obj){
+                return Promise.all([
+                  checkIfPropertyExists(obj, titlegroupId_exists, 'titlegroupId'),
+                  checkIfPropertyExists(obj, accountId_exists, 'accountId'),
+                  checkIfPropertyExists(obj, supplierId_exists, 'supplierId'),
+                  obj])
+                .then(function(array){
+                  title_create(obj, { transaction: tx })
+                      .then(function(obj){
+                        return;
+                      }, function(err){
+                        AnyErrorsHappened = true;
+                        cb(err);
+                      });
+                }, function(err){
+                  AnyErrorsHappened = true;
+                  cb(err);
+                });
+              }, function(err) {
+                AnyErrorsHappened = true;
+                cb(err);
+              })
+              .then(function(result) {
+                return new Promise(function(res,rej) {
+                  if (!AnyErrorsHappened) { // no errors; commit tx
+                    tx.commit(function(err) {
+                      if (err) {
+                        rej(err);
+                      } else {
+                        res(result);
+                      }
                     });
+                  } else { // errors; rollback tx
+                    tx.rollback(function(err) {
+                      if (err) {
+                        rej(err);
+                      } else {
+                        rej();
+                      }
+                    });
+                  }
+                })
+                .then(function(result) { // no errors, return new objects
+                  cb(null,result);
+                }, function(err) { // return err
+                  cb(err);
+                });
+              });
+          });
         }
       };
-
     Title.remoteMethod(
         'DataImport',
       {
