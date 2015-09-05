6315d6d84e1b5d74c41b2108fdf7a3bd8f1b3a48
Emil Virkki <emil.virkki@gmail.com>
2015-09-05 22:48:15 +0300

    Show cost center code when creating a purchase order

--- client/components/NewPurchaseOrder.jsx
+++ client/components/NewPurchaseOrder.jsx
@@ -52,7 +52,7 @@ var getNewPurchaseOrder = function(PurchaseOrderActions, CostCenterStore) {
             <form className="form-horizontal">
               <Input ref="costcenterId" type='select' label='Kustannuspaikka' labelClassName='col-xs-3' wrapperClassName='col-xs-9'>
                 {_.map(this.state.costCenters, function(costCenter) {
-                  return <option value={ costCenter.costcenterId }>{ costCenter.name }</option>
+                  return <option value={ costCenter.costcenterId }>{ costCenter.code } { costCenter.name }</option>
                 })}
               </Input>
               <Input ref="name" type='text' label='Tilauksen nimi' labelClassName='col-xs-3' wrapperClassName='col-xs-9'
