426f19ea907f5e309dd6fb0380c054155ee12818
Masa <matti.sippola@aalto.fi>
2015-09-18 17:35:25 +0300

    Retry, delete the old test

--- e2e-tests/example-test.js
+++
@@ -1,7 +0,0 @@
-describe('my awesome website', function() {
-    it('should do some chai assertions', function() {
-        return browser
-            .url('http://google.com')
-            .getTitle().should.eventually.be.equal('Google');
-    });
-});
