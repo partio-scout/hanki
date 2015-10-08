306cbdb529572e4f448fe3ba1f9e6c2ea61d132c
Emil Virkki <emil.virkki@gmail.com>
2015-10-08 21:51:12 +0300

    Remove duplicate code from dev-setup.sh

--- scripts/dev-setup.sh
+++ scripts/dev-setup.sh
@@ -10,8 +10,8 @@ command="psql template1"
 $command -c "CREATE USER fj16_procurement WITH PASSWORD '$procurement_user_password';"
 $command -c "ALTER USER fj16_procurement CREATEDB;"
 PGPASSWORD="$procurement_user_password" psql template1 fj16_procurement -c "CREATE DATABASE fj16_procurement;"
-node ../server/migrate/create-schema.js
-node ../server/migrate/create-test-fixtures.js
+
+./reset-db.sh
 
 # Create test database
 $command -c "CREATE USER fj16_procurement_test WITH PASSWORD '$procurement_user_password';"
