7579ebb525c0e5cd22a473dc07e5a6f19fb87125
Emil Virkki <emil.virkki@gmail.com>
2015-10-17 23:49:18 +0300

    Move some test utils to shared util file

--- server/test/utils/test-utils.js
+++ server/test/utils/test-utils.js
@@ -1,5 +1,6 @@
 var app = require('../../server');
 var Promise = require('bluebird');
+var expect = require('chai').expect;
 
 function loginUser(username, userpass) {
   userpass = userpass || 'salasana';
@@ -10,4 +11,33 @@ function loginUser(username, userpass) {
   });
 }
 
-module.exports.loginUser = loginUser;
+function createFixture(modelName, fixture, cb) {
+  app.models[modelName].create(fixture, function(err, res) {
+    if (err) {
+      throw new Error('Unable to create ' + modelName + ' fixture: ' + err);
+    } else {
+      cb();
+    }
+  });
+}
+
+function deleteFixtureIfExists(modelName, id, cb) {
+  app.models[modelName].destroyById(id, cb());
+}
+
+function expectModelToBeDeleted(modelName, id, cb) {
+  return function() {
+    app.models[modelName].findById(id, function(err, res) {
+      expect(err).to.be.undefined;
+      expect(res).to.be.null;
+      cb();
+    });
+  };
+}
+
+module.exports = {
+  loginUser: loginUser,
+  createFixture: createFixture,
+  deleteFixtureIfExists: deleteFixtureIfExists,
+  expectModelToBeDeleted: expectModelToBeDeleted
+};
