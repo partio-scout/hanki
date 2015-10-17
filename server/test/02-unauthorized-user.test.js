c0a74ea77d602088f3dc234ef4a59f1f83afc090
Emil Virkki <emil.virkki@gmail.com>
2015-10-17 18:31:03 +0300

    Remove duplicate code from unauthorized user access tests

--- server/test/02-unauthorized-user.test.js
+++ server/test/02-unauthorized-user.test.js
@@ -1,56 +1,23 @@
 var app = require('../server');
 var request = require('supertest');
-//var assert = require('assert');
-//var expect = require('chai').expect;
 
 describe('Unauthenticated user', function() {
-  it('should not be able to access Accounts', function(done) {
-    request(app).get('/api/Accounts')
-      .expect(401)
-      .end(done);
-  });
+  function itShouldDeclineUnauthenticatedAccessTo(endpoint) {
+    it('should not be able to access ' + endpoint, function(done) {
+      request(app).get('/api/' + endpoint)
+        .expect(401)
+        .end(done);
+    });
+  }
 
-  it('should not be able to access Costcenters', function(done) {
-    request(app).get('/api/Costcenters')
-      .expect(401)
-      .end(done);
-  });
-
-  it('should not be able to access Deliveries', function(done) {
-    request(app).get('/api/Deliveries')
-      .expect(401)
-      .end(done);
-  });
-
-  it('should not be able to access Purchaseorderrows', function(done) {
-    request(app).get('/api/Purchaseorderrows')
-      .expect(401)
-      .end(done);
-  });
-
-  it('should not be able to access Purchaseorders', function(done) {
-    request(app).get('/api/Purchaseorders')
-      .expect(401)
-      .end(done);
-  });
-
-  it('should not be able to access Suppliers', function(done) {
-    request(app).get('/api/Suppliers')
-      .expect(401)
-      .end(done);
-  });
-
-  it('should not be able to access Titlegroups', function(done) {
-    request(app).get('/api/Titlegroups')
-      .expect(401)
-      .end(done);
-  });
-
-  it('should not be able to access Titles', function(done) {
-    request(app).get('/api/Titles')
-      .expect(401)
-      .end(done);
-  });
+  itShouldDeclineUnauthenticatedAccessTo('Accounts');
+  itShouldDeclineUnauthenticatedAccessTo('Costcenters');
+  itShouldDeclineUnauthenticatedAccessTo('Deliveries');
+  itShouldDeclineUnauthenticatedAccessTo('Purchaseorderrows');
+  itShouldDeclineUnauthenticatedAccessTo('Purchaseorders');
+  itShouldDeclineUnauthenticatedAccessTo('Suppliers');
+  itShouldDeclineUnauthenticatedAccessTo('Titlegroups');
+  itShouldDeclineUnauthenticatedAccessTo('Titles');
 
   it('should not be able to create new Purchaseuser', function(done) {
     request(app).post('/api/Purchaseusers')
