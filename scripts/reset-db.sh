ed2665667f14932ee9248eb0b7630667713f9e2f
Marko Taponen <marko.taponen@boogiesoftware.com>
2015-10-25 21:08:17 +0200

    Boot script created

--- scripts/reset-db.sh
+++ scripts/reset-db.sh
@@ -2,6 +2,5 @@
 
 cd "$(dirname "$0")"
 
-node ../server/migrate/update-schema.js
-#node ../server/migrate/create-schema.js # drops tables before creating
+node ../server/migrate/create-schema.js # drops tables before creating
 node ../server/migrate/create-test-fixtures.js
