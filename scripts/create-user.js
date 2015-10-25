2c9e61f3789d59ef32624e125c891fbbc790b07f
Miika Hänninen <miika.hanninen@gmail.com>
2015-10-25 23:25:20 +0200

    Promisify create-user.js

--- scripts/create-user.js
+++ scripts/create-user.js
@@ -1,7 +1,14 @@
+var Promise = require('bluebird');
+
 var path = require('path');
 var serverPath = path.resolve(__dirname, '../server');
 var app = require(serverPath + '/server.js');
 var crypto = require('crypto');
+
+var purchaseUser = app.models.Purchaseuser;
+var roleMapping = app.models.RoleMapping;
+var createUser = Promise.promisify(purchaseUser.create, purchaseUser);
+var createRoleMapping = Promise.promisify(roleMapping.create, roleMapping);
 
 var opts = require('commander')
   .usage('Usage: npm run create-user <user email>')
@@ -12,8 +19,15 @@ if (opts.args.length < 1) {
   process.exit(1);
 }
 
-var email = opts.args[0];
+function wrapError(message) {
+  return function(err) {
+    var e = new Error(message);
+    e.innerException = err;
+    throw e;
+  };
+}
 
+var email = opts.args[0];
 var password = crypto.randomBytes(24).toString('hex');
 
 var user = {
@@ -25,23 +39,18 @@ var user = {
   userSection: 'n/a'
 };
 
-app.models.Purchaseuser.create(user, function(err, user) {
-  if (err) {
-    console.error('Can\'t create user:\n', err);
-    process.exit(1);
-  } else {
-    app.models.RoleMapping.create({
+createUser(user).catch(wrapError('Couldn\'t create user!'))
+  .then(function (info) {
+    return createRoleMapping({
       'principalType': 'USER',
       'principalId': user.id,
       'roleId': 1
-    }, function(err, res) {
-      if (err) {
-        console.error('Can\'t create role mapping:\n', err);
-        process.exit(1);
-      } else {
-        console.log('User created:\n', user);
-        process.exit(0);
-      }
-    });
-  }
-});
+    }).catch(wrapError('Couldn\' create role mapping!'));
+  })
+  .then(function () {
+    console.log('User created:\n', user);
+    process.exit(0);
+  }, function(err) {
+    console.error(err.message + '\n', err.innerException);
+    process.exit(1);
+  });
