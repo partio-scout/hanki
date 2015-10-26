e00c1b7e68c04fdebb55a6d046ac7c1668523824
Marko Taponen <marko.taponen@boogiesoftware.com>
2015-10-26 22:52:06 +0200

    Models list separated to a file

--- server/boot/01_update_database.js
+++ server/boot/01_update_database.js
@@ -1,36 +1,21 @@
 module.exports = function updateDatabase(server) {
-  db = server.datasources.db;
   var isTest = process.env.NODE_ENV === 'test';
+  if (isTest) {
+    return;
+  };
 
-  if (!isTest) {
-    // The order of these models is important for database row creation!
-    var modelsToUpdate = [
-      'ACL',
-      'AccessToken',
-      'Role',
-      'RoleMapping',
-      'Purchaseuser',
-      'Title',
-      'Titlegroup',
-      'Account',
-      'Supplier',
-      'Delivery',
-      'Costcenter',
-      'Purchaseorder',
-      'Purchaseorderrow'
-    ];
+  db = server.datasources.db;
+  var modelsToUpdate = require('../models-list.js');
 
-    db.setMaxListeners(40);
-
-    if (!db.isActual(modelsToUpdate)) {
-      db.autoupdate(modelsToUpdate, function(err) {
-        if (err) {
-          console.log('Error: '+err+' when autoupdating models: '+modelsToUpdate);
-          throw err;
-        }
-        console.log('Models: '+modelsToUpdate+' updated.');
-        db.disconnect();
-      });
-    };
+  db.setMaxListeners(40);
+  if (!db.isActual(modelsToUpdate)) {
+    db.autoupdate(modelsToUpdate, function(err) {
+      if (err) {
+        console.error('Error: ' + err + ' when autoupdating models: ' + modelsToUpdate);
+        throw err;
+      }
+      console.log('Models: ' + modelsToUpdate + ' updated.');
+      db.disconnect();
+    });
   };
 };
---
+++ server/models-list.js
@@ -0,0 +1,16 @@
+// The order of these models is important for database row creation!
+module.exports = [
+  'ACL',
+  'AccessToken',
+  'Role',
+  'RoleMapping',
+  'Purchaseuser',
+  'Title',
+  'Titlegroup',
+  'Account',
+  'Supplier',
+  'Delivery',
+  'Costcenter',
+  'Purchaseorder',
+  'Purchaseorderrow'
+];
