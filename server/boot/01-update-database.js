a52c41a6e3f4bccd212e7494653ede970ed9410c
Miika Hänninen <miika.hanninen@gmail.com>
2015-10-31 12:07:09 +0200

    Make boot script 01-update-database asynchronous
    The exported boot script didn't have a callback parameter even though
    the scripts execution is asynchronous. This led to loopback continuing
    with the next bootscript even though this one was still executing.

--- server/boot/01-update-database.js
+++ server/boot/01-update-database.js
@@ -1,21 +1,31 @@
-module.exports = function updateDatabase(server) {
+var Promise = require('bluebird');
+
+module.exports = function updateDatabase(server, cb) {
   var isTest = process.env.NODE_ENV === 'test';
   if (isTest) {
-    return;
+    return cb();
   };
 
-  db = server.datasources.db;
+  var db = server.datasources.db;
+  var isActual = Promise.promisify(db.isActual, db);
+  var autoupdate = Promise.promisify(db.autoupdate, db);
+
   var modelsToUpdate = require('../models-list.js');
 
   db.setMaxListeners(40);
-  if (!db.isActual(modelsToUpdate)) {
-    db.autoupdate(modelsToUpdate, function(err) {
-      if (err) {
-        console.error('Error: ' + err + ' when autoupdating models: ' + modelsToUpdate);
-        throw err;
-      }
-      console.log('Models: ' + modelsToUpdate + ' updated.');
-      db.disconnect();
-    });
-  };
+  isActual(modelsToUpdate).then(function(actual) {
+    if (actual) {
+      console.log('Database models are up to date.');
+      return Promise.resolve();
+    } else {
+      return autoupdate(modelsToUpdate)
+        .then(function() {
+          console.log('Models: ' + modelsToUpdate + ' updated.');
+        }, function(err) {
+          console.log('Error: ' + err + ' when autoupdating models: ' + modelsToUpdate);
+          throw err;
+        });
+    }
+  })
+  .nodeify(cb);
 };
