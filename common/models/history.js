c570f9f14d42fc6c89218c38696c3f351a9bfc96
Lauri Tervonen <lauri.tervonen@aalto.fi>
2015-11-10 21:04:17 +0200

    Add common history generation function

--- common/models/history.js
+++ common/models/history.js
@@ -1,6 +1,15 @@
 module.exports = function(History) {
-  History.beforeRemote('create', function(ctx, purchaseOrder, next) {
-    ctx.args.data.timestamp = (new Date()).toISOString();
-    next();
-  });
+  History.remember = {};
+  History.remember.PurchaseOrder = function(ctx, purchaseorder, event_description) {
+    History.create(
+      {
+        'userId': ctx.req.accessToken.userId,
+        'eventtype': event_description,
+        'comment': purchaseorder.name,
+        'purchaseOrderId': purchaseorder.orderId,
+        'timestamp':(new Date()).toISOString()
+      },
+      function(err,instance) {if (err) {throw err;} console.log(instance);}
+    );
+  };
 };
