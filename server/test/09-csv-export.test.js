919b510a5d906168f8968c58b5c82eaee27112d4
ritvje <jenna.ritvanen@gmail.com>
2015-11-27 16:56:11 +0200

    Add tests to check if CSVExport adds order name and costcenterid to orderrow

--- server/test/09-csv-export.test.js
+++ server/test/09-csv-export.test.js
@@ -25,4 +25,97 @@ describe('CSVExport', function() {
       });
     });
   });
+
+  describe('method', function() {
+
+    before(function(done) {
+      testUtils.createFixture('Costcenter',
+        {
+          "costcenterId": 12,
+          "code": "00001",
+          "name": "Testikustannuspaikka",
+          "approverUserId": 2,
+          "controllerUserId": 3
+        })
+      .then(
+        testUtils.createFixture('Purchaseorder',
+          {
+            "orderId": 12,
+          	"name": "Testitilaus",
+          	"costcenterId": 12,
+          	"usageobjectId": 1,
+          	"subscriberId": 3
+          }
+        )
+      )
+      .then(function() {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+    });
+
+    after(function(done) {
+      testUtils.deleteFixtureIfExists('Costcenter', 12)
+      .then(
+        testUtils.deleteFixtureIfExists('Purchaseorder', 12)
+      )
+      .then(function() {
+        done();
+      })
+      .catch(function(err) {
+        done(err);
+      });
+    });
+
+    afterEach(function(done) {
+      app.models.Purchaseorderrow.destroyById(12, done);
+    });
+
+    it('should add order name and costcenterId to purchaseorderrow', function(done) {
+      var expectedCSV = "'amount','approved','confirmed','controllerApproval','delivered','deliveryId','finished','memo','modified','orderId','orderName','costCenterId','orderRowId','ordered','providerApproval','purchaseOrderNumber','titleId','userSectionApproval','nameOverride','priceOverride','unitOverride' 0,'false','false','false','false',0,'false','2015-07-26 13:40:15.002+03','n/a',12,'Testitilaus',12,'false','false',0,0,'false','n/a',0,'n/a'";
+
+      testUtils.loginUser('procurementAdmin').then(function(accessToken) {
+        testUtils.createFixture('Purchaseorderrow',
+        {
+          "amount": 0,
+          "approved": false,
+          "confirmed": false,
+          "controllerApproval": false,
+          "delivered": false,
+          "deliveryId": 0,
+          "finished": false,
+          "memo": "n/a",
+          "modified": "2015-07-26 13:40:15.002+03",
+          "orderId": 12,
+          "orderRowId": 12,
+          "ordered": false,
+          "providerApproval": false,
+          "purchaseOrderNumber": 0,
+          "titleId": 0,
+          "userSectionApproval": false,
+          "nameOverride": "n/a",
+          "priceOverride": 0,
+          "unitOverride": "n/a"
+        }).then(function() {
+          request(app).post('/api/Purchaseorderrows/CSVExport?access_token=' + accessToken.id )
+          .expect(200)
+          .expect(function(err, res) {
+            if (err) {
+              done(err);
+            } else {
+              expect(res.body.csv).to.equal(expectedCSV);
+            }
+          })
+        })
+        .then(function() {
+          done();
+        })
+        .catch(function(err) {
+          done(err);
+        });
+      });
+    });
+  });
 });
