e30a89bd241ca645c5f86927aad147c5bb3b19c3
ritvje <jenna.ritvanen@gmail.com>
2015-11-29 19:56:47 +0200

    Clean up tests for csv-export

--- server/test/09-csv-export.test.js
+++ server/test/09-csv-export.test.js
@@ -7,6 +7,8 @@ var testUtils = require('./utils/test-utils.js');
 
 describe('CSVExport', function() {
 
+  var testId = 123;
+
   // REST API tests for unauthenticated and authenticated users (nothing posted)
   describe('REST API', function() {
 
@@ -29,26 +31,24 @@ describe('CSVExport', function() {
   describe('method', function() {
 
     before(function(done) {
-      testUtils.createFixture('Costcenter',
-        {
-          'costcenterId': 12,
-          'code': '00001',
-          'name': 'Testikustannuspaikka',
-          'approverUserId': 2,
-          'controllerUserId': 3
-        })
+      var costcenter = {
+        'costcenterId': testId,
+        'code': '00001',
+        'name': 'Testikustannuspaikka',
+        'approverUserId': 2,
+        'controllerUserId': 3
+      };
+      var purchaseorder = {
+        'orderId': testId,
+        'name': 'Testitilaus',
+        'costcenterId': 12,
+        'usageobjectId': 1,
+        'subscriberId': 3
+      };
+      testUtils.createFixture('Costcenter', costcenter)
       .then(
-        testUtils.createFixture('Purchaseorder',
-          {
-            'orderId': 12,
-            	'name': 'Testitilaus',
-            	'costcenterId': 12,
-            	'usageobjectId': 1,
-            	'subscriberId': 3
-          }
-        )
-      )
-      .then(function() {
+        testUtils.createFixture('Purchaseorder', purchaseorder)
+      ).then(function() {
         done();
       })
       .catch(function(err) {
@@ -57,9 +57,9 @@ describe('CSVExport', function() {
     });
 
     after(function(done) {
-      testUtils.deleteFixtureIfExists('Costcenter', 12)
+      testUtils.deleteFixtureIfExists('Costcenter', testId)
       .then(
-        testUtils.deleteFixtureIfExists('Purchaseorder', 12)
+        testUtils.deleteFixtureIfExists('Purchaseorder', testId)
       )
       .then(function() {
         done();
@@ -70,11 +70,11 @@ describe('CSVExport', function() {
     });
 
     afterEach(function(done) {
-      app.models.Purchaseorderrow.destroyById(12, done);
+      app.models.Purchaseorderrow.destroyById(testId, done);
     });
 
     it('should add order name and costcenterId to purchaseorderrow', function(done) {
-      var expectedCSV = "amount','approved','confirmed','controllerApproval','delivered','deliveryId','finished','memo','modified','orderId','orderName','costCenterId','orderRowId','ordered','providerApproval','purchaseOrderNumber','titleId','userSectionApproval','nameOverride','priceOverride','unitOverride' 0,'false','false','false','false',0,'false','2015-07-26 13:40:15.002+03','n/a',12,'Testitilaus',12,'false','false',0,0,'false','n/a',0,'n/a'";
+      var expectedCSV = "amount','approved','confirmed','controllerApproval','delivered','deliveryId','finished','memo','modified','orderId','orderName','costCenterId','orderRowId','ordered','providerApproval','purchaseOrderNumber','titleId','userSectionApproval','nameOverride','priceOverride','unitOverride' 0,'false','false','false','false',0,'false','2015-07-26 13:40:15.002+03','n/a'," + testId + ",'Testitilaus'," + testId + ",'false','false',0,0,'false','n/a',0,'n/a'";
 
       testUtils.loginUser('procurementAdmin').then(function(accessToken) {
         testUtils.createFixture('Purchaseorderrow',
@@ -88,8 +88,8 @@ describe('CSVExport', function() {
             'finished': false,
             'memo': 'n/a',
             'modified': '2015-07-26 13:40:15.002+03',
-            'orderId': 12,
-            'orderRowId': 12,
+            'orderId': testId,
+            'orderRowId': testId,
             'ordered': false,
             'providerApproval': false,
             'purchaseOrderNumber': 0,
