9df0540daeac33141e4299d539af052ce2a9c89c
ritvje <jenna.ritvanen@gmail.com>
2015-12-01 22:26:45 +0200

    Add titleName, unit, titlegroupName and deliverydescription to csv-export

--- common/models/purchaseorderrow.js
+++ common/models/purchaseorderrow.js
@@ -17,15 +17,21 @@ module.exports = function(Purchaseorderrow) {
     var app = require('../../server/server');
     var Purchaseorder = app.models.Purchaseorder;
     var Costcenter = app.models.Costcenter;
+    var Title = app.models.Title;
+    var Titlegroup = app.models.Titlegroup;
+    var Delivery = app.models.Delivery;
     var findAllOrderrows = Promise.promisify(Purchaseorderrow.find, Purchaseorderrow);
     var findOrder = Promise.promisify(Purchaseorder.findById, Purchaseorder);
+    var findTitle = Promise.promisify(Title.findById, Title);
+    var findTitlegroup = Promise.promisify(Titlegroup.findById, Titlegroup);
+    var findDelivery = Promise.promisify(Delivery.findById, Delivery);
     var findCostcenter = Promise.promisify(Costcenter.findById, Costcenter);
     var findOrderrows = findAllOrderrows();
 
     function replaceOrderIdWithNameAndAddCostCenter(orderrow) {
       return findOrder(orderrow.orderId).then(function(order) {
         if (order === null) {
-          var err = new Error('Could not find order with id ' + orderrow.id);
+          var err = new Error('Could not find order with id ' + orderrow.orderId);
           err.status = 422;
           throw err;
         } else {
@@ -39,12 +45,49 @@ module.exports = function(Purchaseorderrow) {
       });
     }
 
+    function addTitleNameAndUnitAndTitlegroupName(orderrow) {
+      return findTitle(orderrow.titleId).then(function(title) {
+        if (title === null) {
+          var err = new Error('Could not find title with id ' + orderrow.titleId);
+          err.status = 422;
+          throw err;
+        } else {
+          return findTitlegroup(title.titlegroupId).then(function(titlegroup) {
+            orderrow.titlegroupName = titlegroup.name;
+            if (title.titlegroupId === 0) { // Muu tuote: nimi ja yksikkö tilausrivistä
+              orderrow.titleName = orderrow.nameOverride;
+              orderrow.titleUnit = unitOverride;
+            } else {
+              orderrow.titleName = title.name;
+              orderrow.titleUnit = title.unit;
+            }
+            return orderrow;
+          });
+        }
+      });
+    }
+
+    function addDeliveryDescription(orderrow) {
+      return findDelivery(orderrow.deliveryId).then(function(delivery) {
+        if (delivery === null) {
+          var err = new Error('Could not find delivery with id ' + orderrow.deliveryId);
+          err.status = 422;
+          throw err;
+        } else {
+          orderrow.deliveryDescription = delivery.description;
+          return orderrow;
+        }
+      });
+    }
+
     function orderrowsToCSV(orderrows) {
-      var fields = [ 'amount', 'approved', 'confirmed', 'controllerApproval', 'delivered', 'deliveryId', 'finished', 'memo', 'modified', 'orderId', 'orderName', 'costCenterCode', 'orderRowId', 'ordered', 'providerApproval', 'purchaseOrderNumber', 'titleId', 'userSectionApproval', 'nameOverride', 'priceOverride', 'unitOverride', 'requestService' ];
+      var fields = [ 'amount', 'approved', 'confirmed', 'controllerApproval', 'delivered', 'deliveryId', 'finished', 'memo', 'modified', 'orderId', 'orderName', 'costCenterCode', 'orderRowId', 'ordered', 'providerApproval', 'purchaseOrderNumber', 'titleId', 'titleName', 'titleUnit', 'titlegroupName', 'userSectionApproval', 'nameOverride', 'priceOverride', 'unitOverride', 'requestService', 'deliveryDescription' ];
       return toCSV({ data: orderrows, fields: fields });
     }
 
     findOrderrows.map(replaceOrderIdWithNameAndAddCostCenter)
+    .map(addTitleNameAndUnitAndTitlegroupName)
+    .map(addDeliveryDescription)
     .then(orderrowsToCSV)
     .nodeify(cb);
 
