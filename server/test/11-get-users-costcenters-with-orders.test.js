d3811debc30e79a4d538e272f5e69fe56c1964e2
ritvje <jenna.ritvanen@gmail.com>
2016-01-16 16:08:41 +0200

    Chage test paths to match remote method path

--- server/test/11-get-users-costcenters-with-orders.test.js
+++ server/test/11-get-users-costcenters-with-orders.test.js
@@ -7,18 +7,18 @@ var testUtils = require('./utils/test-utils.js');
 var Promise = require('bluebird');
 var Purchaseuser = app.models.Purchaseuser;
 var createUser = Purchaseuser.createWithRolesAndCostcenters;
-var costcenter, user, order, orderrow;
 
 describe('getUsersCostcentersWithOrders', function() {
+  var costcenter, user, order, orderrow;
 
   beforeEach(function(done) {
-    costcenter = {
+    var costcenterFixture = {
       'code': '00107',
       'name': 'Testikustannuspaikka',
       'approverUserId': 2,
       'controllerUserId': 3,
     };
-    var purchaseuser = {
+    var userFixture = {
       'memberNumber': '1234567',
       'username': 'test_user',
       'password': 'salasana',
@@ -28,23 +28,23 @@ describe('getUsersCostcentersWithOrders', function() {
       'enlistment': 'n/a',
       'userSection': 'n/a',
     };
-    testUtils.createFixture('Costcenter', costcenter)
-    .then(function(costCenter) {
-      costcenter = costCenter;
+    testUtils.createFixture('Costcenter', costcenterFixture)
+    .then(function(createdCostcenter) {
+      costcenter = createdCostcenter;
       return testUtils.find('Role', { 'name': 'orderer' });
     }).then(function(role) {
-      return createUser(purchaseuser, role, [costcenter], [], []);
-    }).then(function(User) {
-      user = User;
-      order = {
+      return createUser(userFixture, role, [costcenter], [], []);
+    }).then(function(createdUser) {
+      user = createdUser;
+      var orderFixture = {
         'name': 'Testitilaus',
         'costcenterId': costcenter.id,
         'subscriberId': user.id,
       };
-      return testUtils.createFixture('Purchaseorder', order);
-    }).then(function(Order) {
-      order = Order;
-      orderrow = {
+      return testUtils.createFixture('Purchaseorder', orderFixture);
+    }).then(function(createdOrder) {
+      order = createdOrder;
+      var orderrowFixture = {
         'titleId': 1,
         'amount': 2,
         'deliveryId': 1,
@@ -64,9 +64,9 @@ describe('getUsersCostcentersWithOrders', function() {
         'priceOverride': null,
         'unitOverride': null,
       };
-      return testUtils.createFixture('Purchaseorderrow', orderrow);
-    }).then(function(orderRow) {
-      orderrow = orderRow;
+      return testUtils.createFixture('Purchaseorderrow', orderrowFixture);
+    }).then(function(createdOrderrow) {
+      orderrow = createdOrderrow;
     }).nodeify(done);
   });
 
@@ -81,14 +81,14 @@ describe('getUsersCostcentersWithOrders', function() {
 
   describe('REST API', function() {
     it('should decline access for unauthenticated users', function(done) {
-      request(app).get('/api/Purchaseorderrows/getUsersCostcentersWithOrders')
+      request(app).get('/api/Costcenters/getUsersCostcentersWithOrders')
       .expect(401)
       .end(done);
     });
 
     it('should grant access for authenticated users', function(done) {
       testUtils.loginUser('orderer').then(function(accessToken) {
-        request(app).get('/api/Purchaseorderrows/getUsersCostcentersWithOrders')
+        request(app).get('/api/Costcenters/getUsersCostcentersWithOrders')
         .query({ access_token: accessToken.id })
         .expect(200)
         .end(done);
@@ -97,15 +97,12 @@ describe('getUsersCostcentersWithOrders', function() {
   });
 
   describe('method', function() {
-
-    it('should give users costcenters with orders and their orderrows', function(done) {
+    it('should give all orders from users costcenters with orderrows', function(done) {
       var login = testUtils.loginUser(user.username);
       var tempOrder = order.toObject();
-      var tempCostcenter = costcenter.toObject();
       tempOrder.order_rows = [orderrow.toObject()];
-      tempCostcenter.orders = [tempOrder];
       login.then(function(accessToken) {
-        request(app).get('/api/Purchaseorderrows/getUsersCostcentersWithOrders?access_token=' + accessToken.id )
+        request(app).get('/api/Costcenters/getUsersCostcentersWithOrders?access_token=' + accessToken.id )
         .expect(200)
         .end(function(err, res) {
           if (err) {
@@ -113,7 +110,7 @@ describe('getUsersCostcentersWithOrders', function() {
           } else {
             try {
               expect(err).to.be.null;
-              expect(res.body.costcenters).to.contain(tempCostcenter);
+              expect(res.body.orders).to.contain(tempOrder);
               done();
             } catch (e) {
               done(e);
