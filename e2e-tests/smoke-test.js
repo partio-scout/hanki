69a67f1cf66d3df5f55090e21f2d016440278bf1
Emil Virkki <emil.virkki@gmail.com>
2016-02-07 00:09:27 +0200

    Extract e2e-test's app-running logic to separate utility

--- e2e-tests/smoke-test.js
+++ e2e-tests/smoke-test.js
@@ -1,15 +1,4 @@
-// Function expression creating child process is saved to a variable
-var spawn = require('child_process').spawn;
-
-/*
-The new process will get the same enviromental variables as the current
-process, but the PORT is different.
-*/
-var options = process.env;
-options.PORT = 3005;
-
-// A separate process is spawned for tests
-var procSysProcess = spawn('node',['.'],options);
+var appRunner = require('./utils/app-runner');
 
 describe('HANKI', function() {
 
@@ -23,27 +12,10 @@ describe('HANKI', function() {
 
   // According to Mochas hook syntax, be
   before(function(done) {
-    /*
-    An event listener is given for standard output stream of the chill process
-    */
-    procSysProcess.stdout.on('data', function (data) {
-      // Output is transformed into a string
-      var dataString = data.toString();
-      // The keyword on output indecates procurement system is running
-      if (dataString && dataString.indexOf('listening') !== -1){
-        done();
-      }
-    });
+    appRunner.run(done);
   });
 
   after(function(done) {
-    /*
-    A small delay is used before endind tests in order to show the end state
-    of the system for a user
-    */
-    setTimeout(function() {
-      procSysProcess.kill();
-      done();
-    },1000);
+    appRunner.halt(done);
   });
 });
---
+++ e2e-tests/utils/app-runner.js
@@ -0,0 +1,32 @@
+var spawn = require('child_process').spawn;
+
+/*
+The new process will get the same enviromental variables as the current
+process, but the PORT is different.
+*/
+var options = process.env;
+options.PORT = 3005;
+
+var procSysProcess;
+
+function run(cb) {
+  procSysProcess = spawn('node',['.'],options);
+
+  procSysProcess.stdout.on('data', function (data) {
+    var dataString = data.toString();
+    // The keyword on output indecates procurement system is running
+    if (dataString && dataString.indexOf('listening') !== -1){
+      cb();
+    }
+  });
+}
+
+function halt(cb) {
+  procSysProcess.kill();
+  cb();
+}
+
+module.exports = {
+  run: run,
+  halt: halt,
+}
