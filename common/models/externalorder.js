cdc7439f38c5f71bb1462de4543dfb5e9669f008
ritvje <jenna.ritvanen@gmail.com>
2016-03-01 18:55:43 +0200

    Pad externalorderId with zeros so it is easier to use

--- common/models/externalorder.js
+++ common/models/externalorder.js
@@ -1,3 +1,25 @@
-module.exports = function(Externalorder) {
+var _ = require('lodash');
 
+module.exports = function(Externalorder) {
+  Externalorder.afterRemote('*', function(ctx, exOrder, next) {
+    function pad(str, max) {
+      str = str.toString();
+      return str.length < max ? pad('0' + str, max) : str;
+    }
+
+    if (ctx.result) {
+      if (_.isArray(ctx.result)) {
+        ctx.result = _.map(ctx.result, function(order) {
+          order = order.toObject();
+          order.externalorderId = pad(order.externalorderId, 4);
+          return order;
+        });
+        next();
+      } else {
+        ctx.result = ctx.result.toObject();
+        ctx.result.externalorderId = pad(ctx.result.externalorderId, 4);
+        next();
+      }
+    }
+  });
 };
