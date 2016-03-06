1fe2a7397cf356cec1277d4050114ef410036fd8
ritvje <jenna.ritvanen@gmail.com>
2016-03-06 11:12:30 +0200

    Change remote method to remote hook

--- common/models/costcenter.js
+++ common/models/costcenter.js
@@ -1,49 +1,57 @@
 var Promise = require('bluebird');
 var _ = require('lodash');
+var app = require('../../server/server');
 
 module.exports = function(Costcenter) {
-  Costcenter.getTotalPriceOfOrders = function(id, cb) {
-    var findCostcenter = Promise.promisify(Costcenter.findById, Costcenter);
+  Costcenter.afterRemote('**', function(ctx, costcenter, next) {
+    var findOrders = Promise.promisify(app.models.Purchaseorder.find, app.models.Purchaseorder);
 
-    var filter = {
-      include: {
-        relation: 'orders',
-        scope: {
-          include: {
-            relation: 'order_rows',
-            scope: {
-              include: 'title',
-            },
+    function countTotalPriceOfOrders(costcenter) {
+      costcenter = costcenter.toObject();
+      var filter = {
+        where: { costcenterId: costcenter.costcenterId },
+        include: {
+          relation: 'order_rows',
+          scope: {
+            include: 'title',
           },
         },
-      },
-    };
+      };
 
-    findCostcenter(id, filter)
-    .then(function(costcenter) {
-      if (costcenter) {
-        costcenter = costcenter.toObject();
-        var totalPrice = _.reduce(costcenter.orders, function(totalOfOrders, order) {
-          var total = _.reduce(order.order_rows, function(totalOfOrderrows, row) {
-            var title = row.title || { };
-            var titlePrice = row.priceOverride || title.priceWithTax || 0;
-            return totalOfOrderrows + row.amount * titlePrice;
+      return findOrders(filter)
+      .then(function(orders) {
+        if (orders) {
+          var totalPrice = _.reduce(orders, function(totalOfOrders, order) {
+            order = order.toObject();
+            var total = _.reduce(order.order_rows, function(totalOfOrderrows, row) {
+              var title = row.title || { };
+              var titlePrice = row.priceOverride || title.priceWithTax || 0;
+              return totalOfOrderrows + row.amount * titlePrice;
+            }, 0);
+            return totalOfOrders + total;
           }, 0);
-          return totalOfOrders + total;
-        }, 0);
-        return totalPrice;
-      } else {
-        throw new Error('Haluttua kustannuspaikkaa ei löytynyt');
-      }
-    }).nodeify(cb);
-  };
-
-  Costcenter.remoteMethod(
-    'getTotalPriceOfOrders',
-    {
-      http: { path: '/getTotalPriceOfOrders', verb: 'get' },
-      accepts: { arg: 'id', type: 'string' },
-      returns: { arg: 'price', type: 'string' },
+          return totalPrice;
+        } else {
+          return 0;
+        }
+      }).then(function(price) {
+        costcenter.totalPrice = price;
+        return costcenter;
+      });
     }
-  );
+
+    if (ctx.result) {
+      if (_.isArray(ctx.result)) { // handling many costcenters
+        Promise.map(ctx.result, countTotalPriceOfOrders)
+        .then(function(costcenters) {
+          ctx.result = costcenters;
+        }).nodeify(next);
+      } else { // only one costcenter
+        countTotalPriceOfOrders(ctx.result)
+        .then(function(costcenter) {
+          ctx.result = costcenter;
+        }).nodeify(next);
+      }
+    }
+  });
 };
