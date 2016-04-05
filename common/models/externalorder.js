1120580f7f46d785873129851e61a1f896a4bd00
Jenna Ritvanen <jenna.ritvanen@gmail.com>
2016-04-05 23:49:35 +0300

    Implement external orders backend

---
+++ common/models/externalorder.js
@@ -0,0 +1,25 @@
+var _ = require('lodash');
+
+module.exports = function(Externalorder) {
+  Externalorder.afterRemote('*', function(ctx, exOrder, next) {
+    function pad(str, max) {
+      str = str.toString();
+      return str.length < max ? pad('0' + str, max) : str;
+    }
+
+    if (ctx.result) {
+      if (_.isArray(ctx.result)) {
+        ctx.result = _.map(ctx.result, function(order) {
+          order = order.toObject();
+          order.externalorderCode = pad(order.externalorderId, 4);
+          return order;
+        });
+        next();
+      } else {
+        ctx.result = ctx.result.toObject();
+        ctx.result.externalorderCode = pad(ctx.result.externalorderId, 4);
+        next();
+      }
+    }
+  });
+};
